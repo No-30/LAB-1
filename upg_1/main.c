f66aa42f92d89d9e7a4476717ba45f242b2ba33c
Jakob Annetun Sjöberg <jag25002@student.mdu.se>
2026-04-22 13:01:06 +0200

    start på upg_1

---
+++ upg_1/main.c
@@ -0,0 +1,17 @@
+#include <nrfx.h>
+#include <nrf5340_application.h>
+#include <nrfx_config.h>
+#include <nrf.h>
+#include <nrfx_uarte.h>
+#include <stdlib.h>
+#include <nrfx_rtc.h>
+#include <nrfx_systick.h>
+#include <nrf_gpio.h>
+#include <stdHeader.h>
+#include "list.c"
+#include "list.h"
+
+int main(void)
+{ 
+    
+}
