1a11d930921ae0568ff121147881c50f7c6e7f81
Jakob Annetun Sjöberg <jag25002@student.mdu.se>
2026-04-27 14:39:04 +0200

    före test

--- labupg_1/main.c
+++ labupg_1/main.c
@@ -19,8 +19,11 @@ int main(void)
     waitForButton(BUTTON1);
     initRTC();
 
+    List stack = create_empty_list();
+    List queue = create_empty_list();
     //nu är initialisering klar
 
+    char sprintfBuffer[STRING_LENGTH];
     int pressedButton = buttonPressed();
     int rngNum;
 
@@ -32,20 +35,30 @@ int main(void)
             rngNum = rand() % 10 + 1;
             if (rngNum % 2)
             {
-                // enque
+                // enqueue
+                add_last(&queue, rngNum);
             }
             else
             {
                 // push
+                add_last(&stack, rngNum);
             }
             break;
 
         case BUTTON2:
             // pop
+            if (number_of_nodes)
+            {
+                remove_last(&stack, rngNum);
+            }
             break;
 
         case BUTTON3:
             // deque
+            if (number_of_nodes)
+            {
+                remove_first(&stack, rngNum);
+            }
             break;
 
         default:
@@ -53,6 +66,11 @@ int main(void)
         }
 
         // front and peek
+        int front = get_first_element(queue);
+        int peek = get_last_element(stack);
+        sprintf(sprintfBuffer, "\n\r Front: %d\n Peek: %d", front, peek);
+
+        uarte_write(sprintfBuffer);
         delay(1, 's');
     }
 }
