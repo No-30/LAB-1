58630df3828702f51eb3384bf8ea25cf64f9b3c7
Jakob Annetun Sjöberg <jag25002@student.mdu.se>
2026-04-27 14:02:37 +0200

    exklusive stack

--- labupg_1/main.c
+++ labupg_1/main.c
@@ -12,5 +12,47 @@
 
 int main(void)
 { 
+    confButtons();
+    initSystick();
+    initUART();
     
+    waitForButton(BUTTON1);
+    initRTC();
+
+    //nu är initialisering klar
+
+    int pressedButton = buttonPressed();
+    int rngNum;
+
+    while (1)
+    {
+        switch (pressedButton)
+        {
+        case BUTTON1:
+            rngNum = rand() % 10 + 1;
+            if (rngNum % 2)
+            {
+                // enque
+            }
+            else
+            {
+                // push
+            }
+            break;
+
+        case BUTTON2:
+            // pop
+            break;
+
+        case BUTTON3:
+            // deque
+            break;
+
+        default:
+            break;
+        }
+
+        // front and peek
+        delay(1, 's');
+    }
 }
