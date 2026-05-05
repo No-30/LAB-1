9377007cdef82040d7cc5d7c5def7ebc8896a721
Jakob <jannetun@gmail.com>
2026-05-05 21:04:52 +0200

    added BSTREE and hashtable headers in sample

--- sample/main.c
+++ sample/main.c
@@ -4,11 +4,14 @@
 #include <nrf.h>
 #include <nrfx_uarte.h>
 #include <stdlib.h>
+#include <math.h>
 #include <nrfx_rtc.h>
 #include <nrfx_systick.h>
 #include <nrf_gpio.h>
 #include <stdHeader.h>
 #include <list.h>
+#include <BSTree.h>
+#include <hashtable.h>
 
 int main(void)
 { 
