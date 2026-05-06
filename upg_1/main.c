42afec4bc18e8edc3751c7cc266ca9856f1a1dd5
No-30 <jannetun@gmail.com>
2026-05-06 14:22:18 +0200

    fungerande test för bst

--- upg_1/main.c
+++ upg_1/main.c
@@ -22,20 +22,21 @@ int main(void)
     BSTree tree = create_empty_tree();
     int to_insert = 0;
 
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 5; i++)
     {
         to_insert = rand() % 101;
-        sprintf(sprintfBuffer, "\n\rtest %d", to_insert);
-        uarte_write(sprintfBuffer);
-        delay(1000, 'm');
-        if(!find_BST(&tree, to_insert))
+        //sprintf(sprintfBuffer, "\n\rtest %d", to_insert);
+        //uarte_write(sprintfBuffer);
+        if(find_BST(tree, to_insert) == 0)
         {
-            sprintf(sprintfBuffer, "\n\rno dum dum");
-            uarte_write(sprintfBuffer);
+            //sprintf(sprintfBuffer, "\n\rno dum dum");
+            //uarte_write(sprintfBuffer);
             insert_sorted_BST(&tree, to_insert);
         }
         else
-            uarte_write("\n\r you dum dum \n\r");
+        {
+            //uarte_write("\n\r you dum dum \n\r");
+        }
     }
 
     sprintf(sprintfBuffer, "\n\rpreorder");
@@ -50,4 +51,39 @@ int main(void)
     uarte_write(sprintfBuffer);
     print_postorder(tree);
     
+    sprintf(sprintfBuffer, "\n\rnumber of nodes: %d", number_of_nodes_BST(tree));
+    uarte_write(sprintfBuffer);
+
+    sprintf(sprintfBuffer, "\n\rdepth: %d", depth(tree));
+    uarte_write(sprintfBuffer);
+
+    sprintf(sprintfBuffer, "\n\rmin depth: %d", min_depth(tree));
+    uarte_write(sprintfBuffer);
+
+    balance_tree(&tree);
+
+    sprintf(sprintfBuffer, "\n\rbalanced tree:");
+    uarte_write(sprintfBuffer);
+
+    sprintf(sprintfBuffer, "\n\rpreorder");
+    uarte_write(sprintfBuffer);
+    print_preorder(tree);
+
+    sprintf(sprintfBuffer, "\n\rinorder");
+    uarte_write(sprintfBuffer);
+    print_inorder(tree);
+
+    sprintf(sprintfBuffer, "\n\rpostorder");
+    uarte_write(sprintfBuffer);
+    print_postorder(tree);
+
+    sprintf(sprintfBuffer, "\n\rdepth: %d", depth(tree));
+    uarte_write(sprintfBuffer);
+
+    sprintf(sprintfBuffer, "\n\rmin depth: %d", min_depth(tree));
+    uarte_write(sprintfBuffer);
+
+    free_tree(&tree);
+    sprintf(sprintfBuffer, "\n\rTree freed");
+    uarte_write(sprintfBuffer);
 }
