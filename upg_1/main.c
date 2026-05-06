6513a2b761a160d898a21b5e9af1b622f4422891
No-30 <jannetun@gmail.com>
2026-05-06 12:05:43 +0200

    försök till test

--- upg_1/main.c
+++ upg_1/main.c
@@ -16,4 +16,38 @@
 int main(void)
 { 
     initAll();
+    initRTC();
+    srand(rtcTime);
+
+    BSTree tree = create_empty_tree();
+    int to_insert = 0;
+
+    for (int i = 0; i < 20; i++)
+    {
+        to_insert = rand() % 101;
+        sprintf(sprintfBuffer, "\n\rtest %d", to_insert);
+        uarte_write(sprintfBuffer);
+        delay(1000, 'm');
+        if(!find_BST(&tree, to_insert))
+        {
+            sprintf(sprintfBuffer, "\n\rno dum dum");
+            uarte_write(sprintfBuffer);
+            insert_sorted_BST(&tree, to_insert);
+        }
+        else
+            uarte_write("\n\r you dum dum \n\r");
+    }
+
+    sprintf(sprintfBuffer, "\n\rpreorder");
+    uarte_write(sprintfBuffer);
+    print_preorder(tree);
+
+    sprintf(sprintfBuffer, "\n\rinorder");
+    uarte_write(sprintfBuffer);
+    print_inorder(tree);
+
+    sprintf(sprintfBuffer, "\n\rpostorder");
+    uarte_write(sprintfBuffer);
+    print_postorder(tree);
+    
 }
