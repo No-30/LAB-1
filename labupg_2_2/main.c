b84404f28b1a75e161bb9fea8b1025d02f172cbd
No-30 <jannetun@gmail.com>
2026-05-08 09:23:13 +0200

    upg_2.2

--- labupg_2_2/main.c
+++ labupg_2_2/main.c
@@ -13,7 +13,42 @@
 #include <BSTree.h>
 #include <hashtable.h>
 
+
 int main(void)
 { 
     initAll();
+
+    HashTable ht;
+    initTable(&ht);
+
+    int to_insert_key = 0;
+    char value_buffer[20];
+
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        to_insert_key = i;
+        sprintf(value_buffer, "val%d", to_insert_key);  // Enkelt värde baserat på nyckel
+        insert(&ht, to_insert_key*TABLE_SIZE, value_buffer);
+    }
+
+    int getNumber[] = {0, 4, 9};
+    int pretime, posttime;
+
+    for (int i = 0; i < 3; i++)
+    {
+        nrf_systick_val_clear();
+        pretime = nrf_systick_val_get();
+        char* val = get(&ht, getNumber[i]*TABLE_SIZE);
+        posttime = nrf_systick_val_get();
+        posttime = pretime - posttime;
+        
+        if (val) {
+            sprintf(sprintfBuffer, "Get key %d: %s, time: %d ticks\n\r", getNumber[i], val, posttime);
+            uarte_write(sprintfBuffer);
+        } else {
+            sprintf(sprintfBuffer, "Get key %d: NULL (not found), time: %d ticks\n\r", getNumber[i], posttime);
+            uarte_write(sprintfBuffer);
+        }
+    }
+    printTable(&ht);
 }
