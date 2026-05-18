6c3ebd9a326b5b48700739fdd2116882fce87417
Jakob <jannetun@gmail.com>
2026-05-18 10:03:39 +0200

    some test things

---
+++ sample_pc_computable/main.c
@@ -0,0 +1,14 @@
+/*#include <stdlib.h>
+#include <math.h>
+#include <nrfx_rtc.h>
+#include <nrfx_systick.h>
+#include <nrf_gpio.h>
+#include <stdHeader.h>
+#include <list.h>
+#include <BSTree.h>
+#include <hashtable.h>*/
+
+int main(void)
+{ 
+    //initAll();
+}
