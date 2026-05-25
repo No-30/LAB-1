1a44a2b50e6ab4b59b0277307ac167dcfa7a340d
real-Post <ivar.duner@gmail.com>
2026-05-25 16:45:54 +0200

    Guacamole

---
+++ LAB_4_Guacamole/main.c
@@ -0,0 +1,23 @@
+#include <nrfx.h>
+#include <nrf5340_application.h>
+#include <nrfx_config.h>
+#include <nrf.h>
+#include <nrfx_uarte.h>
+#include <stdlib.h>
+#include <math.h>
+#include <nrfx_rtc.h>
+#include <nrfx_systick.h>
+#include <nrf_gpio.h>
+#include <Guacamole.h>
+#include <stdio.h>
+
+int main(void)
+{ 
+    initAll_Mod();
+
+    int alive = 1;
+    game(alive);
+
+    sprintf(sprintfBuffer, "\n\rGame Over :(");
+    uarte_write(sprintfBuffer);
+}
